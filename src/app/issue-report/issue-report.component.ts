314e63631011c8772d0dcc95d7a02639b9d2b478
Hang Nguyen <thuhang.nguyen@relexsolutions.com>
2023-06-27 23:17:29 +0300

    add validations

--- src/app/issue-report/issue-report.component.ts
+++ src/app/issue-report/issue-report.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IssuesService } from '../services/issues.service';
 
 @Component({
@@ -15,18 +15,26 @@ export class IssueReportComponent implements OnInit {
     private formBuilder: FormBuilder,
     private issuesService: IssuesService
   ) {}
-  
+
   ngOnInit(): void {
     this.issueForm = this.formBuilder.group({
-      title: [''],
+      title: ['', Validators.required],
       description: [''],
-      priority: [''],
-      type: [''],
+      priority: ['', Validators.required],
+      type: ['', Validators.required],
     });
   }
 
   addIssue() {
+    if (this.issueForm && this.issueForm.invalid){
+      this.issueForm.markAllAsTouched();
+      return
+    }
     this.issuesService.createIssue(this.issueForm?.value);
+    this.formClose.emit();
+  }
+
+  cancelAddIssue() {
     this.formClose.emit();
   }
 }
